793db36b65694af6e63fe5bd0fcfcaaa9e94a252
Công Phan Lê Thành <21110146@student.hcmute.edu.vn>
2023-03-28 22:45:11 +0700

    Add project files.

---
+++ WinFormsRestaurant/Login.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsRestaurant
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+            textBox1.AutoSize = false;
+            textBox1.Height = 40;
+        }
+    }
+}
